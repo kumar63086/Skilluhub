dff9639201cf1e54943622a838ae24dc39cafa67
kumar866 <kumarborelli0@gmail.com>
2025-05-14 13:52:49 +0530

    secound commit

--- src/controllers/webhooks.js
+++ src/controllers/webhooks.js
@@ -5,28 +5,27 @@ const successResponse = require('../utils/common/success-response');
 const errorResponse = require('../utils/common/error-response');
 const catchAsync = require('../utils/catchAsync/catchAsync');
 
-const CLERK_WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
-const webhook = new Webhook(CLERK_WEBHOOK_SECRET);
 
 // Webhook handler for Clerk events
 const clerkWebhooks = catchAsync(async (req, res) => {
   try {
     // Verify the incoming webhook signature using raw body
-    const payload = webhook.verify(req.body, {
+    const webhook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+    const payload = webhook.verify(JSON.stringify(req.body), {
       'svix-id': req.headers['svix-id'],
       'svix-timestamp': req.headers['svix-timestamp'],
       'svix-signature': req.headers['svix-signature'],
     });
 
-    const { data, type } = payload;
+    const { data, type } = req.body;
 
     switch (type) {
       case 'user.created': {
         const userData = {
           _id: data.id,
-          name: `${data.first_name} ${data.last_name}`,
+          name: data.first_name+' '+data.last_name,
           email: data.email_addresses[0].email_address,
-          imageUrl: data.profile_image_url,
+          imageUrl: data.image_url,
         };
 
         await User.create(userData);
@@ -43,7 +42,7 @@ const clerkWebhooks = catchAsync(async (req, res) => {
         const userData = {
           name: `${data.first_name} ${data.last_name}`,
           email: data.email_addresses[0].email_address,
-          imageUrl: data.profile_image_url,
+          imageUrl: data.image_url,
         };
 
         await User.findByIdAndUpdate(data.id, userData);
--- src/utils/common/success-response.js
+++ src/utils/common/success-response.js
@@ -1,8 +1,15 @@
-const success = {
-    status:"success",
-    message: 'Successfully completed the request',
-    data: {},
-    error: {}
-}
+// const success = {
+//     status:"success",
+//     message: 'Successfully completed the request',
+//     data: {},
+//     error: {}
+// }
 
+// module.exports = success;
+const success = ({ message, data = null }) => ({
+    success: true,
+    message,
+    data,
+  });
+  
 module.exports = success;
